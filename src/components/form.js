8324fb134115135b24f43b9f1a559a738a1d2a91
unknown <SurajsinhSolanki1622@gmail.com>
2021-09-12 11:14:18 -0700

    some changes

--- src/components/form.js
+++ src/components/form.js
@@ -1,15 +1,24 @@
 import React from "react";
 
 class Form extends React.Component{
+    constructor(){
+        super();
+        this.state = { value : "Write your name :" };
+         
+    }
     handleFormSubmit = (event) => {
         event.preventDefault();
         let value = document.querySelector("#name").value;
         alert(value); 
     }
+    handleInputChange = (event) => {
+        this.setState({ value : event.target.value})
+    }
+
     render(){
         return(
             <form onSubmit = {this.handleFormSubmit}>
-                <input id = "name" type = "text"/>
+                <input id = "name" type = "text" value = {this.state.value} onChange = {this.handleInputChange} />
                 <input type = "submit" />
             </form>
         );
